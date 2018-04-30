a0be6f5fd57ca7cddc30d20496c0d461a31a56cd
ryoyaa <ryoya76lima@gmail.com>
2018-04-30 11:39:37 +0900

    登録更新削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/items' => 'items#index'
+  post '/items' => 'items#create'
+  get '/items/new' => 'items#new'
   get '/items/:id' => 'items#show'
+  get '/items/:id/edit' => 'items#edit'
+  patch '/items/:id' => 'items#update'
+  delete '/items/:id' => 'items#destroy'
 end
